533f35eed4b58f8fb39bb18fc0b550f8fd77b97b
NaomiDH03 <naomidh2003@gmail.com>
2024-05-05 21:14:38 -0600

    tabla dsitribucion y lote de materia prima

--- jamjam/models.py
+++ jamjam/models.py
@@ -1,20 +1,12 @@
 from django.db import models
 
-# En esta parte creamos nuestras tablitas
-'''
-ID*
-Detalles
-Status
-Lote_ID (FK)
+# En esta parte creamos nuestras tablita
 
-'''
-
-
-class MateriaPrima(models.Model):
+class RawMaterials(models.Model):
     name = models.CharField(max_length=60, null=False, blank=False )
     supplier = models.CharField(max_length=60, null=False, blank=False)
     united_cost = models.FloatField(null=False, blank=False)
-    reception_date = models.DateTimeField(null=False)
+    created_at = models.DateTimeField(null=False, auto_now_add=True)
     expiration_date = models.DateTimeField(null=False)
     
     def __str__(self):
@@ -24,4 +16,23 @@ class QualityReport(models.Model):
     details = models.CharField(max_length=150)
     status = models.BooleanField(default=False)
     loteID = models.ForeignKey('lote', null=False, blank=True, on_delete=models.SET_NULL) 
+    
+    def __str__(self):
+        return self.details
+    
+class Distribution(models.Model):
+    loteID = models.ForeignKey('lote', null=False, blank=True, on_delete=models.SET_NULL)
+    distribution_date = models.DateTimeField(null=False)
+    destination =  models.CharField(max_length=100, null=False)
+    
+    def __str__(self):
+        return self.destination
+
+class RawMaterialsLote(models.Model):
+    loteID = models.ForeignKey('lote', null=False, blank=True, on_delete=models.SET_NULL)
+    rawMaterialID =  models.ForeignKey('RawMaterials', null=False, blank=True, on_delete=models.SET_NULL)
+    quantity = models.FloatField(null=False)
+    
+    def __str__(self):
+        return self.quantity
     
