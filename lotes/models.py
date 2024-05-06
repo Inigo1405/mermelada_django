bcd653af6b5d6780b997058af28ac7a9266974e9
Iñigo Quintana <quintanadinigo@gmail.com>
2024-05-05 21:37:04 -0600

    ad db tables

--- lotes/models.py
+++ lotes/models.py
@@ -25,11 +25,53 @@ class Product(models.Model):
 
 class Lot(models.Model):
     stock = models.IntegerField()
-    product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, blank=True)
-    expiryDate = models.DateTimeField()
+    product_ID = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, blank=True)
+    expiry_Date = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self) :
         return self.created_at
+
+
+# Tablas Naomi
+class RawMaterial(models.Model):
+    name = models.CharField(max_length=60, null=False, blank=False )
+    supplier = models.CharField(max_length=60, null=False, blank=False)
+    united_cost = models.FloatField(null=False, blank=False)
+    expiry_Date = models.DateTimeField(null=False)
+    is_available = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
+    def __str__(self):
+        return self.name
+    
+    
+class QualityReport(models.Model):
+    description = models.CharField(max_length=150)
+    status = models.BooleanField(default=False)
+    lot_ID = models.ForeignKey('Lot', null=False, blank=True, on_delete=models.SET_NULL) 
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Distribution(models.Model):
+    destination = models.CharField(max_length=70)
+    status = models.BooleanField(default=True)
+    lot_ID = models.ForeignKey('Lot', null=False, blank=True, on_delete=models.SET_NULL) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Lot_RawMaterial(models.Model):
+    material = models.DecimalField(max_digits=10, decimal_places=2)
+    lot_ID = models.ForeignKey('Lot', null=False, blank=True, on_delete=models.SET_NULL) 
+    rowMaterial_ID = models.ForeignKey('RawMaterial', null=False, blank=True, on_delete=models.SET_NULL) 
+    
+    def __str__(self):
+        return self.material
