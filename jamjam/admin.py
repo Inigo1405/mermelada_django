22317b421731000f77651f595b4a8174df4b5168
NaomiDH03 <naomidh2003@gmail.com>
2024-05-05 22:17:50 -0600

    Cambios con error

--- jamjam/admin.py
+++ jamjam/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import RawMaterials, QualityReport, RawMaterialsLote, Distribution 
 
 # Register your models here.
+admin.site.register(RawMaterials)
+admin.site.register(QualityReport)
+admin.site.register(RawMaterialsLote)
+admin.site.register(Distribution)
+
--- jamjam/views.py
+++ jamjam/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import RawMaterials, QualityReport, RawMaterialsLote, Distribution 
 
 # Create your views here.
+def home(request): #se llama 'home'porque es la función de la url que se llama 'home'
+    return render(request,'index.html', {})
+
