b36da1dc1b402fdbb73af97df67b2c97e82e4165
NaomiDH03 <naomidh2003@gmail.com>
2024-05-10 22:19:25 -0600

    Cambios

--- lotes/urls.py
+++ lotes/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ #Lista que tiene el registro de todas las direcciones
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
 ]
+   
--- lotes/views.py
+++ lotes/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request): #se llama 'home'porque es la función de la url que se llama 'home'
     return render(request,'index.html', {})
+
+def login(requets): #Para el html de login
+    return render(requets,'login.html',{})
+
+
+
