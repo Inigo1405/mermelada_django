d302818260d7a37ab7eb3a5782664d6bcd061350
NaomiDH03 <naomidh2003@gmail.com>
2024-05-10 19:42:46 -0600

    Diseño landing, log in y sing up (el sign up no esta muy bien centrado)

--- jamjam/views.py
+++ jamjam/views.py
@@ -5,3 +5,11 @@ from .models import Machine, Product, Lot, RawMaterial, QualityReport, Distribut
 def home(request): #se llama 'home'porque es la función de la url que se llama 'home'
     return render(request,'index.html', {})
 
+def login(requets): #Para el html de login
+    return render(requets,'login.html',{})
+
+def sign(requets): #Para el html de login
+    return render(requets,'sign.html',{})
+
+
+
