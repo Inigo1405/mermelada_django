2fa9936b2d1ced655772670ed86621e4df13dd30
DavidBo9 <194228@iberopuebla.mx>
2024-05-10 23:56:01 -0600

    added  manage products

--- lotes/forms.py
+++ lotes/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Flavor, ProductionLine
+from .models import Flavor, ProductionLine, Product
 
 class FlavorForm(forms.ModelForm):
     class Meta:
@@ -10,3 +10,14 @@ class ProductionLineUpdateForm(forms.ModelForm):
     class Meta:
         model = ProductionLine
         fields = ['progresField', 'is_available']
+        
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'netContent', 'flavor_id']
+        widgets = {
+            'flavor_id': forms.Select(attrs={'class': 'form-control'}),
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'netContent': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
