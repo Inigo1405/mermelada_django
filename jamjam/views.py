c26f0a30978c896aa7c98aa01aa583ea4a4d3cc4
Iñigo Quintana <quintanadinigo@gmail.com>
2024-05-10 23:17:03 -0600

    Delete jamjam

--- jamjam/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Machine, Product, Lot, RawMaterial, QualityReport, Distribution, Lot_RawMaterial
-
-# Create your views here.
-def home(request): #se llama 'home'porque es la función de la url que se llama 'home'
-    return render(request,'index.html', {})
-
