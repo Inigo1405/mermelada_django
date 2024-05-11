10c57b334f4dda9f49193c43ac60483c665c938a
NaomiDH03 <naomidh2003@gmail.com>
2024-05-10 23:15:12 -0600

    Tablita products

--- lotes/urls.py
+++ lotes/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [ #Lista que tiene el registro de todas las direcciones
     path('', views.home, name='home'),
     path('login/', views.login, name='login'),
+    path('products/', views.list_products, name='products')
 ]
    
--- lotes/views.py
+++ lotes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 # Create your views here.
@@ -7,6 +8,11 @@ def home(request): #se llama 'home'porque es la función de la url que se llama 
 
 def login(requets): #Para el html de login
     return render(requets,'login.html',{})
+
+def list_products(request):
+    products = Product.objects.all() #NO SE TE OLVIDE NAOMI: Para traerlos todos
+    return render(request,'products.html',{'products': products}) #Se pone como diccionario porque los guardamos como json
+
 
 
 
