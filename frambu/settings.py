e750a523ebacd6263e9c384e0f03f418fee9265d
Iñigo Quintana <quintanadinigo@gmail.com>
2024-05-13 02:25:46 -0600

    .

--- frambu/settings.py
+++ frambu/settings.py
@@ -131,9 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 #! Cambios para deploy
-STATIC_ROOT = BASE_DIR/"staticfiles"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
